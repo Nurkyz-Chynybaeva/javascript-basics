5be68aa6765ecc71a77070e18df2d8a96f1453f6
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-01-15 14:53:57 +0600

    wrote about my favorite book

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 let first = "Shrek";
-
 let second = first;
-
 let third = second;
-
 let fourth = third;
-
 let fifth = fourth;
+
+let capital ="London";
+let country ="GB";
+
+let city = "Bishkek";
+let kg = "Kyrgyzstan";
+
+
+
+
+let author= "Hal ELdord";
+let book= "Miracle morning";
+
+let secondauthor= "Paulo Koleo";
+let secondbook= "Alchemist";
+
+let subject= "Distance";
+let unit= "meters";
+
+subject= "weight";
+unit= "grammar";
+
+subject= "height";
+unit= "found";
+
 
 console.log(fifth);
 
-console.log("London is the capital of GB");
+console.log(city + " is the capital of " + kg);
+
+console.log(subject + " is measused in " + unit);
+
+console/log("My favorite book is" + book + " by " + author);
 
 
+console/log("My favorite book is" + secondbook + " by " + secondauthor);
 
 
 
